b80069578988c0ed20f355419fa8608e1c70273b
Samar Ali <samarali@live.com>
2016-06-06 12:54:32 +0500

    Initial commit

---
+++ app/src/main/java/com/techiespk/conekt/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.techiespk.conekt;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samar_000 on 6/6/2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+
+        //initializing important things here
+    }
+}
